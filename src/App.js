8216f433a54e747df8d2fbf03180dedd0ff8c525
Alexander Antonov <a.antonov76358@gmail.com>
2020-05-02 08:31:09 +0700

    added favicon

--- src/App.js
+++ src/App.js
@@ -72,6 +72,19 @@ class App extends React.Component {
                   color="primary"
                   onChange={this.handleChange}
                 />
+                <div>
+                  Icons made by{' '}
+                  <a
+                    href="https://www.flaticon.com/authors/pixel-perfect"
+                    title="Pixel perfect"
+                  >
+                    Pixel perfect
+                  </a>{' '}
+                  from{' '}
+                  <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                  </a>
+                </div>
               </React.Fragment>
             )}
           />} />
